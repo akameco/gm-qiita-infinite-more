cf8f9a095d553d0e63461c86386b79a7e743e88b
akameco <akameco.t@gmail.com>
2016-06-02 17:39:37 +0900

    init

---
+++ gm-qiita-infinite-more.user.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name        qiita-road
+// @namespace   qiita
+// @include     http://qiita.com/*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+/* global $ */
+
+function infinitelyMore() {
+	const btn = $('.more-button').get(0);
+
+	if (!btn) {
+		return;
+	}
+
+	const wHeight = window.innerHeight;
+	const wScroll = window.pageYOffset || document.scrollTop;
+
+	const btnOffset = $('.more-button').offset().top;
+	if (wScroll > (btnOffset - wHeight)) {
+		btn.click();
+	}
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+	window.addEventListener('scroll', infinitelyMore);
+	window.addEventListener('resize', infinitelyMore);
+});
---
+++ readme.md
@@ -0,0 +1,16 @@
+# gm-qiita-infinite-more
+
+> Qiitaのフィードにオートリロードを追加するGreaseMonkeyスクリプト
+
+
+## インストール
+
+[インストール](https://raw.githubusercontent.com/akameco/gm-qiita-infinite-more/master/gm-qiita-infinite-more.user.js)
+
+## デモ
+
+![qiita.gif](media/qiita.gif)
+
+## License
+
+MIT © [akameco](http://akameco.github.io)
